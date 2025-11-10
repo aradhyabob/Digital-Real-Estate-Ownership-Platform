b992c51886cb70d759622c72e56a2c66bfd70f36
aradhyabob <xviper582gmail.com>
2025-11-10 20:42:18 +0530

    Add files via upload

---
+++ Server_For_Users/web/static/js/particles.js
@@ -0,0 +1,25 @@
+// Load particles.js and configure settings
+document.addEventListener("DOMContentLoaded", function () {
+    particlesJS("particles-js", {
+      particles: {
+        number: { value: 80, density: { enable: true, value_area: 800 } },
+        color: { value: "#ffffff" },
+        shape: { type: "circle" },
+        opacity: { value: 0.6, random: true },
+        size: { value: 3, random: true },
+        move: { enable: true, speed: 2, direction: "none", out_mode: "out" }
+      },
+      interactivity: {
+        events: {
+          onhover: { enable: true, mode: "repulse" },
+          onclick: { enable: true, mode: "push" }
+        },
+        modes: {
+          repulse: { distance: 100, duration: 0.4 },
+          push: { particles_nb: 4 }
+        }
+      },
+      retina_detect: true
+    });
+  });
+  
